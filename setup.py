2e2e03720140a66a09e0e215eb499527abfeab7b
jcostello93 <jcostello93@gmail.com>
2025-01-31 21:16:21 +0100

    Add jupyter notebook (#7)
    * Setup Jupyter notebooks
    
    * Add pre-commit hook to remove notebook results
    
    * Refactor S3 fetch_file
    
    * Fetch event log from notebook

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_project',
-    version='0.1',
+    name="my_project",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
-        'boto3>=1.36.2,<2.0',
-        'requests>=2.32.3,<3.0',
-        'pm4py==2.7.13.1',
-        'ruff==0.9.4',
-        'pre-commit==4.1.0',
+        "boto3>=1.36.2,<2.0",
+        "requests>=2.32.3,<3.0",
+        "pm4py==2.7.13.1",
+        "ruff==0.9.4",
+        "pre-commit==4.1.0",
+        "jupyter==1.1.1",
+        "nbstripout==0.2.3",
     ],
 )
