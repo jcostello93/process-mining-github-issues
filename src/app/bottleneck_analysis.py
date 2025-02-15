988185989cfc7e122661365139af2f376d17484e
John Costello <jcostello93@gmail.com>
2025-02-15 20:41:23 +0100

    Remove unused charts in bottleneck

--- src/app/bottleneck_analysis.py
+++ src/app/bottleneck_analysis.py
@@ -1,27 +1,7 @@
-import streamlit as st
-import pm4py
-from src.app import case_duration_over_time, dotted_line_chart, dfg
+from src.app import dfg
 
 
 def show(full_log, filtered_log):
     full_log = full_log.copy()
-    performance_spectrum_file_path = "performance_spectrum.png"
 
-    # Event options
-    st.write("🔹 Enter Events (Comma-Separated)")
-    event_string = st.text_area("Event Sequence", "created, not_planned")
-    ordered_events = [e.strip() for e in event_string.split(",") if e.strip()]
-
-    if len(ordered_events) < 2:
-        st.error("Please enter at least two events.")
-
-    pm4py.save_vis_performance_spectrum(
-        filtered_log,
-        ordered_events,
-        file_path=performance_spectrum_file_path,
-    )
-    st.image(performance_spectrum_file_path, use_container_width=False)
-    dotted_line_chart.show(filtered_log)
-
-    case_duration_over_time.show(filtered_log)
     dfg.show(full_log, filtered_log)
