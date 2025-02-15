5a2d56987d7e511ba1676ebf4c1506ec741a6921
John Costello <jcostello93@gmail.com>
2025-02-15 20:42:12 +0100

    Restructure stats grid

--- src/app/stats.py
+++ src/app/stats.py
@@ -9,16 +9,14 @@ from src.app import (
     num_issues_created_over_time,
     num_events_over_time,
     num_open_issues_over_time,
-    pct_closed_over_time,
 )
 
 
-def third_grid(filtered_log):
+def second_grid(filtered_log):
     # Define file paths for images
     file_paths = {
         "Events Over Days of the Week": "events_over_days_of_week.png",
         "Events Over Hour of the Day": "events_over_hour_of_day.png",
-        "Events Over Time": "events_over_time.png",
         "Case Duration Distribution": "case_duration.png",
     }
 
@@ -33,9 +31,6 @@ def third_grid(filtered_log):
         distr_type="hours",
         file_path=file_paths["Events Over Hour of the Day"],
     )
-    pm4py.save_vis_events_per_time_graph(
-        filtered_log, file_path=file_paths["Events Over Time"]
-    )
     pm4py.save_vis_case_duration_graph(
         filtered_log, file_path=file_paths["Case Duration Distribution"]
     )
@@ -46,27 +41,19 @@ def third_grid(filtered_log):
 
     # Display images in the grid
     with col1:
+        num_events_over_time.show(filtered_log)
+    with col2:
+        num_issues_created_over_time.show(filtered_log)
+    with col3:
         st.image(
             file_paths["Events Over Days of the Week"],
             caption="Events Over Days of the Week",
             use_container_width=True,
         )
-    with col2:
+    with col4:
         st.image(
             file_paths["Events Over Hour of the Day"],
             caption="Events Over Hour of the Day",
-            use_container_width=True,
-        )
-    with col3:
-        st.image(
-            file_paths["Events Over Time"],
-            caption="Events Over Time",
-            use_container_width=True,
-        )
-    with col4:
-        st.image(
-            file_paths["Case Duration Distribution"],
-            caption="Case Duration Distribution",
             use_container_width=True,
         )
 
@@ -80,39 +67,17 @@ def first_grid(filtered_log):
 
     # First Chart: Issues Created Over Time
     with col1:
-        num_active_cases_over_time.show(filtered_log)
+        dotted_line_chart.show(filtered_log)
 
     # Placeholder for additional charts
     with col2:
         case_duration_distribution.show(filtered_log)
 
     with col3:
-        num_events_over_time.show(filtered_log)
+        num_active_cases_over_time.show(filtered_log)
 
     with col4:
-        num_issues_created_over_time.show(filtered_log)
-
-
-def second_grid(filtered_log):
-    filtered_log = filtered_log.copy()
-
-    # Create a 2x2 grid layout
-    col1, col2 = st.columns(2)
-    col3, col4 = st.columns(2)
-
-    # First Chart: Issues Created Over Time
-    with col1:
-        pct_closed_over_time.show(filtered_log)
-
-    # Placeholder for additional charts
-    with col2:
-        dotted_line_chart.show(filtered_log)
-
-    # with col3:
-    #     # num_active_cases_over_time.show(filtered_log)
-
-    # with col4:
-    #     # num_events_over_time.show(filtered_log)
+        num_open_issues_over_time.show(filtered_log)
 
 
 def show(filtered_log):
@@ -125,9 +90,7 @@ def show(filtered_log):
     case_dispersion_ratio = get_case_dispersion_avg(cell_log)
 
     # Display Metrics
-    st.write(
-        f"📏 **Median Case Duration:** {median_case_duration // 60 // 60 // 24} days"
-    )
+    st.write(f"📏 **Median Case Duration:** {median_case_duration // 60 // 60} hours")
     st.write(
         f"⏳ **Avg. Time Between Case Arrivals:** {case_arrival_average // 60 // 60} hours"
     )
@@ -137,4 +100,3 @@ def show(filtered_log):
 
     first_grid(filtered_log)
     second_grid(filtered_log)
-    third_grid(filtered_log)
