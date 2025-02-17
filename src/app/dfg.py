e83d3efbea8dab34cc5d181ccbd12bc7b677e55b
John Costello <jcostello93@gmail.com>
2025-02-17 14:07:57 +0100

    Reorient DFG and add endpoints filter

--- src/app/dfg.py
+++ src/app/dfg.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pm4py
 from pm4py.algo.filtering.dfg.dfg_filtering import filter_dfg_on_activities_percentage
 from pm4py.algo.filtering.dfg.dfg_filtering import filter_dfg_on_paths_percentage
-from pm4py.visualization.dfg import visualizer as dfg_visualizer
-from pm4py.algo.discovery.dfg.variants import clean_time as clean_dfg_time
-from pm4py.visualization.dfg.variants import timeline as timeline_gviz_generator
 from src.app import evaluate, sample_util
 
 
@@ -12,18 +9,29 @@ def show(full_log, filtered_log):
     full_log = full_log.copy()
     st.title("DFG")
 
+    endpoints = {"closed", "not_planned"}
+    selected_endpoints = st.multiselect(
+        "Filter endpoints",
+        sorted(list(endpoints)),
+        default=sorted(list(endpoints)),
+    )
+
+    if selected_endpoints:
+        filtered_log = pm4py.filtering.filter_end_activities(
+            filtered_log, selected_endpoints
+        )
+
     # Discover the frequency DFG using activites and paths to filter
     activities = pm4py.get_event_attribute_values(filtered_log, "concept:name")
     frequency_dfg, start_activities, end_activities = pm4py.discover_dfg(filtered_log)
+
     activities_perc = st.slider(
         "Activities Percentage", min_value=0.0, max_value=1.0, value=0.90, step=0.05
     )
     paths_perc = st.slider(
         "Paths Percentage", min_value=0.0, max_value=1.0, value=0.10, step=0.05
     )
-    max_num_edges = st.slider(
-        "Max Number of Edges", min_value=1, max_value=500, value=100, step=1
-    )
+
     frequency_dfg, start_activities, end_activities, activities = (
         filter_dfg_on_activities_percentage(
             frequency_dfg, start_activities, end_activities, activities, activities_perc
@@ -35,7 +43,7 @@ def show(full_log, filtered_log):
         )
     )
 
-    # Discover the performance DFG (does not support activites and paths filtering)
+    # Discover the performance DFG (does not support activities and paths filtering)
     performance_dfg, start_activities, end_activities = pm4py.discover_performance_dfg(
         filtered_log
     )
@@ -57,8 +65,7 @@ def show(full_log, filtered_log):
         frequency_dfg,
         start_activities,
         end_activities,
-        max_num_edges=max_num_edges,
-        rankdir="LR",
+        rankdir="TB",
         file_path="frequency_dfg.svg",
         format="svg",
     )
@@ -67,7 +74,7 @@ def show(full_log, filtered_log):
         start_activities,
         end_activities,
         aggregation_measure="sum",
-        rankdir="LR",
+        rankdir="TB",
         file_path="performance_dfg_sum.svg",
         format="svg",
     )
@@ -76,7 +83,7 @@ def show(full_log, filtered_log):
         start_activities,
         end_activities,
         aggregation_measure="median",
-        rankdir="LR",
+        rankdir="TB",
         file_path="performance_dfg_median.svg",
         format="svg",
     )
@@ -88,28 +95,10 @@ def show(full_log, filtered_log):
         pnet, pim, pfm = pm4py.convert_to_petri_net(
             frequency_dfg, start_activities, end_activities
         )
-        # petri_filename = "petri_dfg_net.svg"
-        # pm4py.save_vis_petri_net(pnet, pim, pfm, file_path=petri_filename, format="svg")
-        # st.image(petri_filename, use_container_width=True)
+        petri_filename = "petri_dfg_net.svg"
+        pm4py.save_vis_petri_net(pnet, pim, pfm, file_path=petri_filename, format="svg")
+        st.image(petri_filename, use_container_width=True)
         evaluate.show(sample_log, pnet, pim, pfm)
 
     st.image("performance_dfg_sum.svg", use_container_width=False)
     st.image("performance_dfg_median.svg", use_container_width=False)
-
-    try:
-        dfg_time = clean_dfg_time.apply(filtered_log)
-        gviz = timeline_gviz_generator.apply(
-            frequency_dfg,
-            dfg_time,
-            parameters={
-                "max_no_of_edges_in_diagram": max_num_edges,
-                "start_activities": start_activities,
-                "end_activities": end_activities,
-            },
-        )
-        dfg_filename = "dfg_timeline.png"
-        dfg_visualizer.save(gviz, dfg_filename)
-
-        st.image(dfg_filename, use_container_width=False)
-    except Exception:
-        print("Could not load timeline")
